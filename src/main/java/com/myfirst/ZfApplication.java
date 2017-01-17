4a68973499e44df8c90c1d8ed6e17e7d8b4a2ca4
yunzhifei <yunzhifei@58ganji.com>
2017-01-17 13:14:43 +0800

    m

---
+++ src/main/java/com/myfirst/ZfApplication.java
@@ -0,0 +1,12 @@
+package com.myfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZfApplication.class, args);
+	}
+}
