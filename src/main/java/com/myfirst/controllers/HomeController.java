b0718acab197388353779396c933d9783ae09a35
yunzhifei <yunzhifei@58ganji.com>
2017-01-17 14:43:45 +0800

    @PathVariable 测试成功

--- src/main/java/com/myfirst/controllers/HomeController.java
+++ src/main/java/com/myfirst/controllers/HomeController.java
@@ -2,8 +2,11 @@ package com.myfirst.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by 58 on 2017/1/17.
@@ -17,9 +20,11 @@ public class HomeController {
         return "index";
     }
 
-    @RequestMapping(value = "/hello")
-    public String hello(Model model) {
-        model.addAttribute("hello",6);
+    @RequestMapping(value = "/hello/{userId}")
+    public String hello(Model model, HttpServletRequest request, @PathVariable("userId") String userId) {
+        System.out.println(request.getRequestURL());
+        System.out.println(userId);
+        model.addAttribute("hello", 6);
         return "hello";
     }
 
--- src/test/java/com/myfirst/ZfApplicationTests.java
+++ src/test/java/com/myfirst/ZfApplicationTests.java
@@ -13,7 +13,7 @@ public class ZfApplicationTests {
 
     @Test
     public void contextLoads() {
-
+        
     }
 
 }
